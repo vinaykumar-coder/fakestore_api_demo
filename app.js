b84dba5062c3babd822bf35c8f1cb7ffde7244b7
vinaykumar-coder <vinaykumar12264@gmail.com>
2024-10-28 21:06:12 +0530

    created the div, image and p tags using DOM manipulation in js.

--- app.js
+++ app.js
@@ -9,3 +9,27 @@ async function fetchProducts() {
     console.log("Error while fetching the products", error);
   }
 }
+// display products on web page using dom manipulation
+
+function displayProducts(products){
+    const productContainer =  document.getElementById('product-container');
+
+    products.forEach(product =>{
+        const productCard = document.createElement('div');
+        productCard.classList.add('product-card')
+
+
+        const productImage = document.createElement('img')
+        productImage.src = product.image;
+        productImage.alt = product.title;
+
+        const productTitle = document.createElement('h2');
+        productTitle.textContent = product.title;
+
+        const productDescription = document.createElement('p')
+        productDescription.textContent = `${product.description.slice(0, 100)}...`;
+
+        const productPrice = document.createElement('span')
+        productPrice.textContent = `Price: $₹{product.price}`;
+    })
+}
